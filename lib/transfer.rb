41c7f276d4e6554124b7cc0e2210096bd844b237
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-29 23:00:40 -0500

    commit tree 555c5de58a0b127a78dbee20b66cd177f0ce7bec

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  
+
+  def initialize(sender, receiver, transfer)
+    @receiver = receiver
+    @sender = sender
+    @status = "pending"
+    @amount = transfer
+  end
+
+  def valid?
+      
+    if sender.valid? && receiver.valid? #why does this work if #valid? is an instance method of bank_account
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if valid? && @sender.balance > @amount && @status == "pending"
+    sender.balance -= @amount
+    receiver.balance += @amount
+    @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+    sender.balance += @amount
+    receiver.balance -= @amount
+    @status = "reversed"
+    else
+      nil
+    end
+  end
 end
